5ab45929df63b9bdc38a447927f9d9c5cbe64762
axinterop <axineop@gmail.com>
2021-09-24 16:55:11 +0200

    Added content to 'password reset done' template
    Added tests for 'password reset done' view

--- accounts/tests/test_view_password_reset.py
+++ accounts/tests/test_view_password_reset.py
@@ -68,3 +68,16 @@ class InvalidPasswordResetTests(TestCase):
     def test_no_reset_email_sent(self):
         # During test running, each outgoing email is saved in django.core.mail.outbox
         self.assertEqual(0, len(mail.outbox))
+
+
+class PasswordResetDoneTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset_done')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/done/')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
